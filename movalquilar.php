8f0addf19d93e9381a6395c4940480a39de2cdc9
daVinciManuel <mgmt.manuel02@gmail.com>
2025-01-28 13:23:35 +0100

    create controllers to mov-files

--- movalquilar.php
+++ movalquilar.php
@@ -1,22 +1,5 @@
 <?php
-require './movconfig.php';
-require './sql/connect.php';
-require './model/queries.php';
-require './model/verifyLogin.php';
-require './cookies/checkToken.php';
-if (!checkToken()) {
-    header('Location: ./movlogin.php');
-} else {
-    // VALORES to HTML
-    $name = ($_COOKIE['name']);
-    $fullname = getFullName($name);
-    $id = getIdcliente($name);
-    $logoutURL = './control/logout.php';
-    $alquilarURL = './movalquilar.php';
-    $consultarURL = './movconsultar.php';
-    $devolverURL = './movdevolver.php';
-    $fechayhora = date('Y-m-d H:i');
-}
+require './control/calquilar.php';
 ?>
 <html>
    

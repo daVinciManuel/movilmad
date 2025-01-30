c9fe927f57401cc80a5230efdd874665ba7b6bc5
daVinciManuel <mgmt.manuel02@gmail.com>
2025-01-30 20:12:57 +0100

    Create sql/passwordsTable.sql

---
+++ sql/passwordsTable.sql
@@ -0,0 +1,4 @@
+USE movilmad;
+CREATE TABLE topsecret ( idcliente integer(5) not null, clave varchar(100) not null) ENGINE=InnoDB;
+ALTER TABLE topsecret ADD CONSTRAINT pk_topsecret PRIMARY KEY (idcliente,clave);
+ALTER TABLE topsecret ADD CONSTRAINT fk_topsecret FOREIGN KEY (idcliente) references rclientes(idcliente);

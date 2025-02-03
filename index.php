4131014bc39e7f67620bb729e2c1b6352e9e37b9
daVinciManuel <mgmt.manuel02@gmail.com>
2025-02-03 08:47:32 +0100

    fixing headers problems

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
+
 require './movconfig.php';
 require './sql/connect.php';
 require './model/queries.php';
 require './model/verifyLogin.php';
 require './cookies/checkToken.php';
-if(!checkToken()){
-  header('Location: ./movlogin.php');
-}else{
-  header('Location: ./movwelcome.php');
+if (!checkToken()) {
+    header('Location: ./movlogin.php');
+} else {
+    header('Location: ./movwelcome.php');
 }
-?>

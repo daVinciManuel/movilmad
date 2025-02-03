9632e75873fc2ba0e5375e9b7c1e153524e3111f
daVinciManuel <mgmt.manuel02@gmail.com>
2025-02-03 23:07:32 +0100

    fix headers in cconsultar.php

--- control/cconsultar.php
+++ control/cconsultar.php
@@ -1,23 +1,22 @@
 <?php
-require './movconfig.php';
+
+// require './movconfig.php';
 require './sql/connect.php';
 require './model/queries.php';
 require './model/verifyLogin.php';
 require './cookies/checkToken.php';
-ob_start();
-if(!checkToken()){
-  header('Location: ./movlogin.php');
-}else{
-  // VALORES to HTML
-  $name = ($_COOKIE['name']);
-  $fullname = getFullName($name);
-  $id = getIdcliente($name);
-  $logoutURL = './control/logout.php';
-  $alquilarURL = './movalquilar.php';
-  $consultarURL = './movconsultar.php';
-  $devolverURL = './movdevolver.php';
+if (!checkToken()) {
+    header('Location: ./movlogin.php');
+} else {
+    // VALORES to HTML
+    $name = ($_COOKIE['name']);
+    $fullname = getFullName($name);
+    $id = getIdcliente($name);
+    $logoutURL = './control/logout.php';
+    $alquilarURL = './movalquilar.php';
+    $consultarURL = './movconsultar.php';
+    $devolverURL = './movdevolver.php';
 }
-if(isset($_POST['volver'])){
-  header('Location: ./movwelcome.php');
+if (isset($_POST['volver'])) {
+    header('Location: ./movwelcome.php');
 }
-ob_end_flush();
